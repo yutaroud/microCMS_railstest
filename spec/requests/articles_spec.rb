9f75e67bf1d7534cfd339f15ca6d0f850a7df020
Yuki <yutaroud@yahoo.co.jp>
2023-02-07 02:24:47 +0900

    記事一覧ページのステータステスト

---
+++ spec/requests/articles_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "Articles", type: :request do 
+  describe '記事の一覧表示が可能か' do
+    it '記事一覧を表示する' do
+      get '/'
+      expect(response.status).to eq 200
+    end
+  end
+end
