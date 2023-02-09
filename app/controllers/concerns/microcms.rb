09e1c89241fa3c0bfa47e838af494ebeea9108da
Yuki <yutaroud@yahoo.co.jp>
2023-02-10 00:16:39 +0900

    一覧取得と個別取得でmethodを分ける

--- app/controllers/concerns/microcms.rb
+++ app/controllers/concerns/microcms.rb
@@ -3,17 +3,36 @@ module Microcms
   require 'httpclient'
 
   PAGE_LIMIT = 10.freeze
+  CMS_API_PATH = "https://#{Rails.application.credentials.microcms[:domain]}.microcms.io/api/v1/blogs/"
 
-  def article_list(offset=nil, id=nil, draft_key=nil)
+  def article_list(offset=nil)
     client = HTTPClient.new
-    response = client.get(
-      "https://#{Rails.application.credentials.microcms[:domain]}.microcms.io/api/v1/blogs/#{id.present? ? id : ''}",
-      header: { "X-MICROCMS-API-KEY": Rails.application.credentials.microcms[:key] },
-      query: { draftKey: draft_key, offset: offset }
-    )
-    
-    return nil unless response.status == 200
+    response = client.get(CMS_API_PATH, header: cms_header, query: list_query)
+
+    return raise StandardError.new("リクエストが失敗しました。#{response.body}") unless response.status == 200
 
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def article_show(id, draft_key=nil)
+    client = HTTPClient.new
+    response = client.get("#{CMS_API_PATH}#{id}", header: cms_header, query: show_query(draft_key))
+    
+    return raise StandardError.new("リクエストが失敗しました。#{response.body}") unless response.status == 200
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+    def cms_header
+      { "X-MICROCMS-API-KEY": Rails.application.credentials.microcms[:key] }
+    end
+
+    def list_query
+      { offset: offset }
+    end
+
+    def show_query(draft_key)
+      { draftKey: draft_key }
+    end
 end
